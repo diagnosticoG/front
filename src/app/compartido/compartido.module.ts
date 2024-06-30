c76c5ed43c2c1e5474e01eededae78bbd9fc28be
Sebastian Ilbay <atlantiscityplay@gmail.com>
2024-06-30 13:45:21 -0500

    contenedor compartido
    Se ha creado un componente contenedor que será reutilizable para los módulos de gestión CRUD

--- src/app/compartido/compartido.module.ts
+++ src/app/compartido/compartido.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TablaComponent } from './tabla/tabla.component';
 import { MatTableModule } from '@angular/material/table';
+import { ContenedorComponent } from './contenedor/contenedor.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
-    TablaComponent
+    TablaComponent,
+    ContenedorComponent
   ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class CompartidoModule { }
